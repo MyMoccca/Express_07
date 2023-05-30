abd72133c76ae38b98d62a1ed6078d557467f054
Marta Miszke <mishke.design@gmail.com>
2023-05-30 11:34:49 +0200

    creation authentification

--- app.js
+++ app.js
@@ -9,26 +9,24 @@ const welcome = (req, res) => {
 };
 
 app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-const { hashPassword } = require("./auth.js");
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
 
 app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 
-
-argon2
-  .hash(password, hashingOptions)
-  .then((hashedPassword) => {
-    // do something with hashedPassword
-  })
-  .catch((err) => {
-    // do something with err
-  });
 
 app.listen(port, (err) => {
   if (err) {

7348ecb9346c03397fc2cb68613f453d86fbaa25
Marta Miszke <mishke.design@gmail.com>
2023-06-04 11:11:59 +0200

    finalisation exercice get post update

--- app.js
+++ app.js
@@ -14,14 +14,12 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
 
 
 app.post("/api/users", hashPassword, userHandlers.postUser);
